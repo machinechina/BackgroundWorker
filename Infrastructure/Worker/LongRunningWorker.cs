0ff7b2e4921434e27724b165dc3b4c69ce6428df
majun <majun@32d91889-fa02-9e4d-b8fd-8fc4873ef940>
2017-07-20 05:15:08 +0000

    native util for Devicechange and UNC connection

---
+++ Infrastructure/Worker/LongRunningWorker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure.Threading;
+using static Infrastructure.Threading.IntervalTask;
+
+namespace Infrastructure.Workers
+{
+    public class LongRunningWorker : Worker
+    {
+        private Action _action;
+
+        public LongRunningWorker(Action action, int loopInterval) : base(loopInterval, stopAfterContinuousIdleLoopCount: -1)
+        {
+            _action = action;
+        }
+
+        protected override IntervalTask.WorkingState DoWork()
+        {
+            _action();
+            return WorkingState.BUSY;
+        }
+    }
+}
+
+
+
